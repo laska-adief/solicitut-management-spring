f6cdd8d55e7cf646c442231c1c01a944efae4d0a
laska-adief <laskaadief.la@gmail.com>
2025-02-08 14:17:25 +0700

    create CustomerRepository

---
+++ src/main/java/com/solicitut/management/repositories/customer/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.solicitut.management.repositories.customer;
+
+import com.solicitut.management.models.customer.CustomerModel;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends ReactiveCrudRepository<CustomerModel, UUID> {
+  Flux<CustomerModel> findAllBy(Pageable pageable);
+}
