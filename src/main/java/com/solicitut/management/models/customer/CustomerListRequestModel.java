39719eb0015c05ce515f8b650d1944e4ca926bb7
laska-adief <laskaadief.la@gmail.com>
2025-02-09 09:39:23 +0700

    add property filterCustomerStatus in CustomerListRequestModel

--- src/main/java/com/solicitut/management/models/customer/CustomerListRequestModel.java
+++ src/main/java/com/solicitut/management/models/customer/CustomerListRequestModel.java
@@ -10,8 +10,10 @@ import lombok.Setter;
 @Setter
 public class CustomerListRequestModel extends ListRequestModel {
   String filterCustomerName;
-  public CustomerListRequestModel(int size, int page, String sortName, String sortValue, String filterCustomerName) {
+  String filterCustomerStatus;
+  public CustomerListRequestModel(int size, int page, String sortName, String sortValue, String filterCustomerName, String filterCustomerStatus) {
     super(size, page, sortName, sortValue);
     this.filterCustomerName = filterCustomerName;
+    this.filterCustomerStatus = filterCustomerStatus;
   }
 }
